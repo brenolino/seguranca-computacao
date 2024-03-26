40f38a26ad4e8d718c0ef9ccb6aefdfed96979ff
Breno <breno.lino@ice.ufjf.br>
2024-03-26 17:42:34 -0300

    add cifra de cesar

---
+++ cifra-cesar.py
@@ -0,0 +1,29 @@
+import string
+
+letras = list(string.ascii_lowercase)
+frase = input("Digite a frase para aplicar a criptografia: ").lower()
+valorValido = False
+
+while valorValido is not True:
+    try:
+        chave = int(input("Valor da chave K para deslocamento: "))
+        valorValido = True
+
+    except ValueError:
+        print("Valor inválido.")
+
+fraseCriptografada = ''
+
+for letra in frase:
+    if letra == " ":
+        fraseCriptografada += " "
+        continue
+
+    posicao = letras.index(letra) + chave
+
+    if posicao > len(letras):
+        posicao = posicao - len(letras)
+
+    fraseCriptografada += letras[posicao]
+
+print(f"Frase criptografada = {fraseCriptografada}")
