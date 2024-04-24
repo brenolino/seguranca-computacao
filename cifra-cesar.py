abbdd1ccc11882a8d087d6786b5c86af8f951657
Breno <breno.lino@ice.ufjf.br>
2024-04-24 18:15:48 -0300

    add cifra de cesar corrigido

--- cifra-cesar.py
+++ cifra-cesar.py
@@ -7,6 +7,8 @@ valorValido = False
 while valorValido is not True:
     try:
         chave = int(input("Valor da chave K para deslocamento: "))
+
+        # Verifica a validade da chave de entrada.
         if chave < 26 and chave >= 0:
             valorValido = True      
             continue  
@@ -19,6 +21,7 @@ while valorValido is not True:
 
 fraseCriptografada = ''
 
+# Realiza a troca das letras de acordo com a chave.
 for letra in frase:
     if letra == " ":
         fraseCriptografada += " "
@@ -33,7 +36,7 @@ for letra in frase:
 
 print(f"Frase criptografada = {fraseCriptografada}")
 
-# Criptoanálise
+# Contabiliza a frequencia da letra no texto.
 def calcularFrequencia(texto):
     frequencia = {}
     totalCaracteres = len(texto.replace(" ", ""))
@@ -53,4 +56,35 @@ def calcularFrequencia(texto):
 
     return frequencia
 
-print(calcularFrequencia(fraseCriptografada))
+def criptoanalise(texto):
+
+    # Frequência de letras no alfabeto português.
+    frequenciaEsperada = {'a': 0.1463, 'b': 0.0104, 'c': 0.0388, 'd': 0.0499, 'e': 0.1257, 'f': 0.0102,
+                           'g': 0.0130, 'h': 0.0128, 'i': 0.0618, 'j': 0.0040, 'k': 0.0002, 'l': 0.0278,
+                           'm': 0.0474, 'n': 0.0505, 'o': 0.1073, 'p': 0.0252, 'q': 0.0120, 'r': 0.0653,
+                           's': 0.0781, 't': 0.0434, 'u': 0.0463, 'v': 0.0167, 'w': 0.0001, 'x': 0.0021,
+                           'y': 0.0001, 'z': 0.0047}
+    
+    frequenciaTextoCriptografado = calcularFrequencia(texto)
+
+    menorDiferenca = float('inf')
+    melhorChave = None
+
+    for chave in range(26):
+        diferencaTotal = 0
+        
+        # Calcula a soma das diferenças entre as frequências observadas e esperadas para cada letra.
+        for letra in frequenciaTextoCriptografado:
+            letraDeslocada = chr(((ord(letra) - ord('a') + chave) % 26) + ord('a'))
+            frequenciaObservada = frequenciaTextoCriptografado[letra]
+            frequenciaEsperadaLetra = frequenciaEsperada[letraDeslocada]
+            diferencaTotal += abs(frequenciaObservada - frequenciaEsperadaLetra)
+        
+        # Verifica se a soma das diferenças é menor que a menor diferença encontrada até agora.
+        if diferencaTotal < menorDiferenca:
+            menorDiferenca = diferencaTotal
+            melhorChave = chave
+    
+    return melhorChave
+
+print("Chave de deslocamento prevista com base na frequência:", chave)
